df54ce7d81557f2ea7fc2069fdef58ac92a0f579
morais244 <morais.n@escolar.ifrn.edu.br>
2025-11-03 11:30:41 -0300

    a
    sdsas

--- jogo/Assets/codigo/Arma.cs
+++ jogo/Assets/codigo/Arma.cs
@@ -3,41 +3,67 @@ using UnityEngine;
 public class Arma : MonoBehaviour
 {
     public Transform saidaDoTiro;
-    public GameObject balaPrefab;
-    public float intervaloDeDisparo = 0.25f;
-    public float velocidadeBala = 10f;
-
-    private float tempoDeDisparo;
-    private Camera cam;
-
-    void Start()
+    public GameObject bala;
+    public float intevaloDeDisparo = 0.2f;
+    
+    private float tempoDeDisparo = 0;
+    
+    private Camera camera;
+    public GameObject cursor;
+  void Start()
     {
-        cam = Camera.main;
+        camera = Camera.main;
     }
-
-    void Update()
+   void Update()
     {
-        // Mira
-        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direcao = mousePos - transform.position;
-        float angulo = Mathf.Atan2(direcao.y, direcao.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angulo);
-
-        // Atira
-        if (Input.GetMouseButton(0) && Time.time > tempoDeDisparo)
+         
+        if (gameObject.transform.rotation.eulerAngles.z > -90 
+            && gameObject.transform.rotation.eulerAngles.z < 90)
         {
-            tempoDeDisparo = Time.time + intervaloDeDisparo;
-            GameObject bala = Instantiate(balaPrefab, saidaDoTiro.position, saidaDoTiro.rotation);
-            bala.GetComponent<Rigidbody2D>().linearVelocity = saidaDoTiro.right * velocidadeBala;
+            transform.localScale = new Vector3(1 , 1, 1);
+        }
+        
+        if (gameObject.transform.rotation.eulerAngles.z > 90 
+            && gameObject.transform.rotation.eulerAngles.z < 270)
+        {
+            transform.localScale = new Vector3( 1, -1, 1);
         }
 
+
         
-    }
-    void OnDrawGizmos()
-    {
-    Gizmos.color = Color.red;
-    Gizmos.DrawLine(transform.position, transform.position + transform.right * 1f);
+        
+        // Distância da câmera ao objeto. Precisamos disso para fazer o cálculo correto.
+        float camDis = camera.transform.position.y - transform.position.y;
+
+        // Obtém a posição do mouse no espaço mundial. Usando camDis para o eixo Z.
+        Vector3 mouse = camera.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, camDis));
+
+        float AngleRad = Mathf.Atan2 (mouse.y - transform.position.y, mouse.x - transform.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+
+        transform.rotation =  Quaternion.AngleAxis( angle, Vector3.forward);
+       
+        Debug.Log("Angilo: "+angle);
+        
+        cursor.transform.position = new Vector3(mouse.x, mouse.y, cursor.transform.position.z);
+        
+        Debug.DrawLine(transform.position, mouse , Color.red);
+        
+        
+        if (tempoDeDisparo <=0 && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+            Debug.Log("Bala disparada");
+         
+            GameObject b = Instantiate (this.bala,saidaDoTiro.position, saidaDoTiro.rotation) as GameObject;
+            
+            tempoDeDisparo = intevaloDeDisparo;
+        }
+
+        if (tempoDeDisparo > 0)
+        {
+            tempoDeDisparo -= Time.deltaTime;
+        }
+
     }
 }
-
-
--- jogo/Assets/codigo/Player.cs
+++ jogo/Assets/codigo/Player.cs
@@ -1,71 +1,67 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class Player : Personagem
 {
-    public float velocidade = 5f;
-    public int vidaMaxima = 100;
-    public int vidaAtual;
-    public int pontuacao;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    
+    public Transform arma;
 
-    public Slider barraVidaUI;
-    public TMPro.TextMeshProUGUI textoPontuacao;
-
-    private Rigidbody2D rb;
-    private Vector2 direcao;
-
-    void Start()
+    private bool amdando;
+    
+   void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        vidaAtual = vidaMaxima;
-        AtualizarHUD();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
-
-    void Update()
+  void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-        direcao = new Vector2(moveX, moveY).normalized;
-        AtualizarHUD();
-    }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + direcao * velocidade * Time.fixedDeltaTime);
-    }
-
-    public void LevarDano(int dano)
-    {
-        vidaAtual -= dano;
-        if (vidaAtual <= 0)
+        amdando = false;
+        
+        //direita
+        if (arma.rotation.eulerAngles.z > -90 
+            && arma.rotation.eulerAngles.z < 90)
         {
-            vidaAtual = 0;
-            AtualizarHUD();
-            Morrer();
-            return;
+            spriteRenderer.flipX = false;
+        }
+        
+        //esquerda
+        if (arma.rotation.eulerAngles.z > 90 
+            && arma.rotation.eulerAngles.z < 270)
+        {
+            spriteRenderer.flipX = true;
         }
 
-        AtualizarHUD();
-    }
 
-    void Morrer()
-    {
-        // Aqui você pode adicionar animação ou efeitos antes de destruir
-        Destroy(gameObject);
-    }
 
-    public void AdicionarPontuacao(int pontos)
-    {
-        pontuacao += pontos;
-        AtualizarHUD();
-    }
+        //movimento para a esquerda
+        if (Input.GetKey(KeyCode.A))
+        {
+          transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0, 0);  
+          amdando = true;
+        }
 
-    void AtualizarHUD()
-    {
-        if (barraVidaUI != null)
-            barraVidaUI.value = (float)vidaAtual / vidaMaxima;
-
-        if (textoPontuacao != null)
-            textoPontuacao.text = $"{pontuacao}";
+        //movimento para a direita
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
+            amdando = true;
+        }
+        
+        //movimento para a Cima
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0); 
+            amdando = true;
+        }
+        
+        //movimento para a Cima
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0);  
+            amdando = true;
+        }
+        
+        animator.SetBool("Andando", amdando);
     }
 }
